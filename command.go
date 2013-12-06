91a847987037265d20f522e474c841565516c42e
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2013-12-06 12:23:25 +0100

    better formatting of errors, notices...

--- command.go
+++ command.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 )
 
 // look up the absolute path to where bash is installed on the host
@@ -26,6 +30,9 @@ type Command struct {
 // if a process is already running, it will kill that one
 // and before spawning a new one
 func (c *Command) Execute() {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
 	if c.isRunning {
 		log.Println("restarting process...")
 		if err := c.process.Process.Kill(); err != nil {
@@ -36,12 +43,33 @@ func (c *Command) Execute() {
 	c.isRunning = true
 
 	c.process = exec.Command(getBash(), "-c", c.Cmd)
-	c.process.Stderr = os.Stderr
-	c.process.Stdin = os.Stdin
-	c.process.Stdout = os.Stdout
 
-	if err := c.process.Run(); err != nil {
-		log.Println("failed to run process.")
+	c.process.Stdout = &stdout
+	c.process.Stderr = &stderr
+	c.process.Stdin = os.Stdin
+
+	err := c.process.Run()
+
+	clock := time.Now().Format("15:04:05")
+
+	if err != nil {
+		fmt.Printf("Command failed... [%s]\n", clock)
+		fmt.Printf("----------------------------\n")
+
+		if strings.TrimSpace(err.Error()) != "" {
+			fmt.Printf("%s\n", err)
+		}
+	} else {
+		fmt.Printf("Running command... [%s]\n", clock)
+		fmt.Printf("-----------------------------\n")
+	}
+
+	if strings.TrimSpace(stdout.String()) != "" {
+		fmt.Printf("%s\n", stdout.String())
+	}
+
+	if strings.TrimSpace(stderr.String()) != "" {
+		fmt.Printf("%s\n", stderr.String())
 	}
 
 	c.isRunning = false
